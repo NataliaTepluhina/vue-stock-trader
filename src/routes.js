958dcc2ea4c1e9dc7def64bfffe15b721545854a
NataliaTepluhina <tarya.se@gmail.com>
2017-09-03 20:53:09 +0300

    - changed routes to static imports

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,7 @@
 import Home from './components/Home.vue'
+import Portfolio from './components/portfolio/Portfolio.vue'
+import Stocks from './components/stock/Stocks.vue'
 
-const Portfolio = resolve => {
-    require.ensure(['./components/portfolio/Portfolio.vue'], () => {
-        resolve(require('./components/portfolio/Portfolio.vue'));
-    });
-};
-
-const Stocks = resolve => {
-    require.ensure(['./components/stock/Stocks.vue'], () => {
-        resolve(require('./components/stock/Stocks.vue'));
-    });
-};
 
 export const routes = [
     {path: '/', component: Home},
